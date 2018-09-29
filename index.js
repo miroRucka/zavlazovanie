a532579529b477630d345347f2e720a92ebc2b28
rucka <miroslav.rucka@posam.sk>
2018-09-29 12:38:46 +0200

    first

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ var gpiop = gpio.promise;
 
 var stompService = require('./stompService')();
 
-var destination = '/topic/take-photo';
+var destination = '/queue/zavlazovanie';
 var stompMessageClient;
 
-stompService.connect(function (sessionId, client) {
-    stompMessageClient = client;
-    client.subscribe(destination, function (body, headers) {
-        console.log("get message ", body);
+var connect = function () {
+    stompService.connect(function (sessionId, client) {
+        stompMessageClient = client;
+        client.subscribe(destination, function (body, headers) {
+            console.log("get message ", body);
+            return gpiop.write(7, body);
+        });
     });
-});
+};
 
 gpiop.setup(7, gpio.DIR_OUT)
-    .then(function () {
-        return gpiop.write(7, true)
-    }).catch(function (err) {
+    .then(connect).catch(function (err) {
     console.log('Error: ', err.toString())
 });
