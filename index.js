bc706d5a90fbca3bb2f7bef758e4f52e02da93b5
rucka <miroslav.rucka@posam.sk>
2018-09-29 19:15:42 +0200

    first

--- index.js
+++ index.js
@@ -19,15 +19,39 @@ var err = function (err) {
     console.log('Error: ', err.toString())
 };
 
+var subscribe = function (client, destination, pin) {
+    client.subscribe(destination, function (body, headers) {
+        console.log("get message ", body);
+        return gpiop.write(pin, body);
+    });
+};
+
+var unsubscribe = function (client, destination) {
+    client.unsubscribe(destination);
+};
+
 var initPin = function (pin, client, destination) {
     gpiop.setup(pin, gpio.DIR_OUT).then(function () {
         console.log('pin ', pin, ' setup done.');
-        client.subscribe(destination, function (body, headers) {
-            console.log("get message ", body);
-            return gpiop.write(pin, body);
-        });
+        subscribe(client, destination, pin);
     }).catch(err);
 };
+
+var resubscribe = function () {
+    unsubscribe(stompMessageClient, dest);
+    subscribe(stompMessageClient, dest, 7);
+};
+
+
+var scheduler = function () {
+    setTimeout(function () {
+        console.log('start repair script');
+        resubscribe();
+        scheduler();
+    }, 3000);
+};
+
+scheduler();
 
 
 /**
@@ -35,7 +59,11 @@ var initPin = function (pin, client, destination) {
  */
 process.on('SIGINT', function () {
     gpio.destroy();
-    stompMessageClient.disconnect();
+    try {
+        stompMessageClient.disconnect();
+    } catch (e) {
+        console.log('error', e);
+    }
 });
 process.on('uncaughtException', function (err) {
     console.log('Caught exception: ', err);
