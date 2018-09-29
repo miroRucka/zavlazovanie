61e8b92130668ba9d9c021c1f2831ebbf088c1ab
rucka <miroslav.rucka@posam.sk>
2018-09-29 16:12:38 +0200

    first

--- index.js
+++ index.js
@@ -9,22 +9,25 @@ var stompService = require('./stompService')();
 var destination = '/topic/zavlazovanie';
 var stompMessageClient;
 
-stompService.connect(function (sessionId, client) {
-    console.log('connected ...');
-    stompMessageClient = client;
-});
-
 var err = function (err) {
     console.log('Error: ', err.toString())
 };
 
-gpiop.setup(7, gpio.DIR_OUT).then(function () {
-    console.log('pin 7 setup done.');
+var writeToPin = function (pin, value) {
+    gpiop.setup(pin, gpio.DIR_OUT).then(function () {
+        console.log('pin ', pin, ' setup done.');
+        return gpiop.write(pin, value);
+    }).catch(err);
+};
+
+stompService.connect(function (sessionId, client) {
+    console.log('connected ...');
+    stompMessageClient = client;
     stompMessageClient.subscribe(destination, function (body, headers) {
         console.log("get message ", body);
-        return gpiop.write(7, body);
+        writeToPin(7, body);
     });
-}).catch(err);
+});
 
 //**************************************
 //*********** REST API *****************
