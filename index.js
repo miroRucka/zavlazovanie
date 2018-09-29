54e250e3127df21965944914da6437d5b4e9adc2
rucka <miroslav.rucka@posam.sk>
2018-09-29 16:29:51 +0200

    first

--- index.js
+++ index.js
@@ -6,32 +6,35 @@ var gpiop = gpio.promise;
 
 var stompService = require('./stompService')();
 
-var destination = '/topic/zavlazovanie';
+var dest = '/topic/zavlazovanie';
 var stompMessageClient;
 
 stompService.connect(function (sessionId, client) {
     console.log('connected ...');
     stompMessageClient = client;
+    initPin(7, client, dest)
 });
 
 var err = function (err) {
     console.log('Error: ', err.toString())
 };
 
-gpiop.setup(7, gpio.DIR_OUT).then(function () {
-    console.log('pin 7 setup done.');
-    stompMessageClient.subscribe(destination, function (body, headers) {
-        console.log("get message ", body);
-        return gpiop.write(7, body);
-    });
-}).catch(err);
+var initPin = function (pin, client, destination) {
+    gpiop.setup(pin, gpio.DIR_OUT).then(function () {
+        console.log('pin ', pin, ' setup done.');
+        client.subscribe(destination, function (body, headers) {
+            console.log("get message ", body);
+            return gpiop.write(pin, body);
+        });
+    }).catch(err);
+};
 
 
 /**
  * after crash process I close gpio
  */
 process.on('SIGINT', function () {
-    gpio.destroy(function() {
+    gpio.destroy(function () {
         console.log('All pins unexported');
     });
     stompMessageClient.disconnect();
