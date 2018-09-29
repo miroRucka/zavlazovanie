b281a75fe86127c52f5868c938a4f75a3f7a0f87
rucka <miroslav.rucka@posam.sk>
2018-09-29 16:23:52 +0200

    first

--- index.js
+++ index.js
@@ -9,24 +9,35 @@ var stompService = require('./stompService')();
 var destination = '/topic/zavlazovanie';
 var stompMessageClient;
 
+stompService.connect(function (sessionId, client) {
+    console.log('connected ...');
+    stompMessageClient = client;
+});
+
 var err = function (err) {
     console.log('Error: ', err.toString())
 };
 
-var writeToPin = function (pin, value) {
-    gpiop.setup(pin, gpio.DIR_OUT).then(function () {
-        console.log('pin ', pin, ' setup done.');
-        return gpiop.write(pin, value);
-    }).catch(err);
-};
-
-stompService.connect(function (sessionId, client) {
-    console.log('connected ...');
-    stompMessageClient = client;
+gpiop.setup(7, gpio.DIR_OUT).then(function () {
+    console.log('pin 7 setup done.');
     stompMessageClient.subscribe(destination, function (body, headers) {
         console.log("get message ", body);
-        writeToPin(7, body);
+        return gpiop.write(7, body);
     });
+}).catch(err);
+
+
+/**
+ * after crash process I close gpio
+ */
+process.on('SIGINT', function () {
+    gpio.destroy(function() {
+        console.log('All pins unexported');
+    });
+    stompMessageClient.disconnect();
+});
+process.on('uncaughtException', function (err) {
+    logger.error('Caught exception: ', err);
 });
 
 //**************************************
