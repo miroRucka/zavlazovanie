d7a55b132d4a05f54d52a1d85bb47a587cbe1074
rucka <miroslav.rucka@posam.sk>
2018-09-29 15:19:22 +0200

    first

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var gpio = require('rpi-gpio')
+var gpio = require('rpi-gpio');
+var express = require('express');
+
+var app = express();
 var gpiop = gpio.promise;
 
 var stompService = require('./stompService')();
 
-var destination = '/queue/zavlazovanie';
+var destination = '/topic/zavlazovanie';
 var stompMessageClient;
 
 var connect = function () {
@@ -20,3 +23,13 @@ gpiop.setup(7, gpio.DIR_OUT)
     .then(connect).catch(function (err) {
     console.log('Error: ', err.toString())
 });
+
+//**************************************
+//*********** REST API *****************
+//**************************************
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.listen(3000)
